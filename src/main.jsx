605cc251374d56c79e89a6695f2b123f9d25a236
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2025-11-03 13:20:42 +0530

    Add main.jsx entry point

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ImageViewer from './ImageViewer.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ImageViewer />
+  </React.StrictMode>,
+)
